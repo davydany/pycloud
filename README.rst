c8b70a8f3df5d34933960469deae7e8f28e8a5cd
David Daniel <davydany@gmail.com>
2018-08-02 23:14:02 -0400

    Cleaned up certain behaviors. Added docs

--- README.rst
+++ README.rst
@@ -2,19 +2,43 @@
 PyCloud
 =========
 
+.. image:: https://travis-ci.org/davydany/pycloud.svg?branch=master
+    :target: https://travis-ci.org/davydany/pycloud
 
 
+What it Does
+------------
 
+A simple cloud provisioner for provisioning VMs on EC2 and configuring them
+based on a simple plan.
 
+Getting Started
+---------------
 
-PyCloud
+.. code:: bash
 
+  pip install --upgrade pycloud
 
+Usage
+-----
 
-Features
---------
+To see all the options available for you, run:
 
-* TODO
+.. code:: bash
+
+    pycloud --help
+
+PyCloud uses a plan file to setup your infrastructure, similar to how 
+Ansible works. There are a few sample files in **example_plans** directory.
+
+Here is how to execute a plan:
+
+.. code:: bash
+
+    export AWS_ACCESS_KEY="<YOUR_AWS_ACCESS_KEY>"
+    export AWS_SECRET_KEY="<YOUR_AWS_SECRET_KEY>"
+
+    pycloud ./example_plans/test_plan.yml
 
 Credits
 -------
--- pycloud/pycloud/cli.py
+++ pycloud/pycloud/cli.py
@@ -9,22 +9,10 @@ from pycloud.pycloud.provisioners.plan_executor import PlanExecutor
 
 logger = get_logger()
 
-
-@click.group()
-@click.option('-a', '--access-key', envvar='AWS_ACCESS_KEY',
-              help='AWS Access Key. You can also set environment variable AWS_ACCESS_KEY.')
-@click.option('-s', '--secret-key', envvar='AWS_SECRET_KEY',
-              help='AWS Secret Key. You can also set environment variable AWS_SECRET_KEY.')
-@click.option('-l', '--log-level', envvar='PYCLOUD_LOG_LEVEL',
-              help='Set the Log Level for the command. Default: INFO',
-              type=click.Choice([l.lower() for l in ALLOWED_LOG_LEVELS]),
-              default='INFO')
-@click.pass_context
-def pycloud(ctx, access_key, secret_key, log_level):
-    """
-    PyCloud Provisions a VM in the cloud,
-    """
-    ctx.obj = {}
+def validate_for_aws(access_key, secret_key):
+    '''
+    Validates the provided aws access_key and secret_key.
+    '''
     if not access_key:
         click.secho(
             'Please provide AWS Access Key either as a parameter "--access-key=VALUE" '
@@ -38,34 +26,54 @@ def pycloud(ctx, access_key, secret_key, log_level):
             fg='red')
         raise click.BadParameter('Secret Key is not set.')
 
-    configure_logger(level=log_level)
-    logger.debug("Access KEY: %s" % (access_key))
-    logger.debug("Secret KEY: %s" % ("*" * len(secret_key)))
 
-    ctx.obj['AWS_ACCESS_KEY'] = access_key
-    ctx.obj['AWS_SECRET_KEY'] = secret_key
+
+@click.group()
+@click.option('-l', '--log-level', envvar='PYCLOUD_LOG_LEVEL',
+              help='Set the Log Level for the command. Default: info',
+              type=click.Choice([l.lower() for l in ALLOWED_LOG_LEVELS]),
+              default='info')
+@click.pass_context
+def pycloud(ctx, log_level):
+    """
+    PyCloud Provisions a VM in the cloud,
+    """
+    ctx.obj = {}
+    configure_logger(level=log_level)
     return 0
 
 @pycloud.command()
+@click.option('-a', '--access-key', envvar='AWS_ACCESS_KEY',
+              help='AWS Access Key. You can also set environment variable AWS_ACCESS_KEY.')
+@click.option('-s', '--secret-key', envvar='AWS_SECRET_KEY',
+              help='AWS Secret Key. You can also set environment variable AWS_SECRET_KEY.')
 @click.argument('plan', type=click.Path(exists=True))
 @click.pass_context
-def execute(ctx, plan):
+def execute(ctx, access_key, secret_key, plan):
     '''
     Sets up the infrastructure as specified by the plan.
     '''
+    validate_for_aws(access_key, secret_key)
+    ctx.obj['AWS_ACCESS_KEY'] = access_key
+    ctx.obj['AWS_SECRET_KEY'] = secret_key
     executor = PlanExecutor(plan, _globals=ctx.obj)
     executor.execute_plan()
 
 
 @pycloud.command()
+@click.option('-a', '--access-key', envvar='AWS_ACCESS_KEY',
+              help='AWS Access Key. You can also set environment variable AWS_ACCESS_KEY.')
+@click.option('-s', '--secret-key', envvar='AWS_SECRET_KEY',
+              help='AWS Secret Key. You can also set environment variable AWS_SECRET_KEY.')
 @click.argument('plan', type=click.Path(exists=True))
 @click.pass_context
-def dry_run(ctx, plan):
+def dry_run(ctx, access_key, secret_key, plan):
     '''
     Sets up the infrastructure requested in the CLI.
-    :param ctx:
-    :return:
     '''
+    validate_for_aws(access_key, secret_key)
+    ctx.obj['AWS_ACCESS_KEY'] = access_key
+    ctx.obj['AWS_SECRET_KEY'] = secret_key
     executor = PlanExecutor(plan, _globals=ctx.obj)
     executor.dry_execute_plan()
 
